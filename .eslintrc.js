51e243d7fae5f791447f5819cdb45ddda1d4bd8e
Zhichao Zhang <zzcwoshizz@163.com>
2020-11-03 15:59:46 +0800

    feat: config my config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+const base = require('./packages/config/lib/eslint');
+
+module.exports = {
+  ...base,
+  parserOptions: {
+    ...base.parserOptions,
+    project: [
+      './tsconfig.json'
+    ]
+  }
+};
